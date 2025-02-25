62f5929f40d95ca573ad54e546e4e34284f4328c
hijin-dong <primary_dong@naver.com>
2025-02-25 19:05:36 +0900

    add direction shift script

--- unity/johnLemons-HauntedJaunt/john/Assets/UnityTechnologies/Scripts/PlayerMovement.cs
+++ unity/johnLemons-HauntedJaunt/john/Assets/UnityTechnologies/Scripts/PlayerMovement.cs
@@ -7,16 +7,15 @@ public class PlayerMovement : MonoBehaviour
     Animator m_Animator;
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -28,5 +27,14 @@ public class PlayerMovement : MonoBehaviour
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
     }
 }

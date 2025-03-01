1fe8a86aefd94525146046587b021ba0b945098d
hijin-dong <primary_dong@naver.com>
2025-03-01 23:36:02 +0900

    reset variable when restart game

--- unity/johnLemons-HauntedJaunt/john/Assets/UnityTechnologies/Scripts/GameManager/GameEnding.cs
+++ unity/johnLemons-HauntedJaunt/john/Assets/UnityTechnologies/Scripts/GameManager/GameEnding.cs
@@ -24,7 +24,7 @@ public class GameEnding : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        m_GameFinished = false;
     }
 
     // Update is called once per frame

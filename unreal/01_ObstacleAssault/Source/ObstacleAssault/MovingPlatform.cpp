f5519a3ce766ed4563829bfe39446f99bbc6c99e
hijin-dong <primary_dong@naver.com>
2025-03-14 12:43:48 +0900

    extract platform movement logic into a function

--- unreal/01_ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
+++ unreal/01_ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -24,18 +24,16 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// Move platform forwards
-		// Get current location
-	FVector CurrentLocation = GetActorLocation();
-		// Add vector to that location
-	CurrentLocation = CurrentLocation + PlatformVelocity * DeltaTime;
-		// Set the location
-	SetActorLocation(CurrentLocation);
+	MovePlatform(DeltaTime);
+}
 
-	// Send platform back if gone too far
-		// Check how far we've moved
+void AMovingPlatform::MovePlatform(float DeltaTime)
+{
+	FVector CurrentLocation = GetActorLocation();
+	CurrentLocation = CurrentLocation + PlatformVelocity * DeltaTime;
+	SetActorLocation(CurrentLocation);
 	float DistanceMoved = FVector::Dist(CurrentLocation, StartLocation);
-		// Reverse direction of motion if gone too far
+
 	if (DistanceMoved > LimitedDistance)
 	{
 		FVector MoveDirection = PlatformVelocity.GetSafeNormal();

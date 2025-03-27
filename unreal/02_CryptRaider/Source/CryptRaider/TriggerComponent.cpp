575e737c54834bada793fa9e152b44cab77b2f06
hijin-dong <primary_dong@naver.com>
2025-03-27 19:43:39 +0900

    feat: add trigger component to moving wall (exclude umap)

---
+++ unreal/02_CryptRaider/Source/CryptRaider/TriggerComponent.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TriggerComponent.h"
+
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Display, TEXT("TEST"));
+}
